0e70d1b984f39bf0938d4091b1f23e81cad94b73
AnHoangbk19 <an.phambk19@hcmut.edu.vn>
2022-11-29 14:31:54 +0700

    update db

--- hotel_db.sql
+++ hotel_db.sql
@@ -54,3 +54,22 @@ INSERT INTO `news` (`userPost`, `postTime`, `content`, `newsID`) VALUES
 ('Nguyễn Hữu Nhân', '2022-11-29 08:07:22', 'Insert news1 content', '1234'),
 ('Lê Trí Nguyên', '2022-11-29 08:07:22', 'Insert news2 content', '5678'),
 ('Hoàng An', '2022-11-29 08:07:22', 'Insert news3 content', '1256');
+
+-- Create Rooms Table 
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `roomName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `bath` int(11) NOT NULL,
+  `bed` int(11) NOT NULL,
+  `rating` double NOT NULL,
+  `price` double NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- insert value
+INSERT INTO `rooms` (`id`, `roomName`, `description`,`bath`, `bed`, `rating`, `price`, `image`) VALUES
+(1, 'King Room', 'Luxious that impress even royalties', 3, 2, 5.0, 300000, 'https://i.pinimg.com/originals/ba/9a/c5/ba9ac50e4ea914da0c57fadb558ed4f1.jpg'),
+(2, 'Nature Relaxing Room', 'Enjoy natural beauty along with our services', 2, 2, 4.8, 80000, 'https://images.squarespace-cdn.com/content/v1/4fcf5c8684aef9ce6e0a44b0/1624127208154-VVYP8K5W9LGOJ7PRQ0G7/Living+Room+Example'),
+(3, 'Aqua Room', 'Experience Living in the ocean', 2, 2, 4.7, 150000, 'https://cmsapi.manilaoceanpark.com/storage/uploads/Aqua%20Supreme%201(2)_1567482953_thumbnail.jpg');
