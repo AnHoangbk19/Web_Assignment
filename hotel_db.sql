e08dfb976ef742bac1499d1df13074bf3bcbc44a
AnHoangbk19 <an.phambk19@hcmut.edu.vn>
2022-11-29 14:02:40 +0700

    add database

---
+++ hotel_db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE HOTEL_DB;
+USE HOTEL_DB;
+
+
+CREATE TABLE Booking(
+    Email varchar(255) NOT NULL PRIMARY KEY,
+    Name varchar(255) NOT NULL,
+    Check_in varchar(255) NOT NULL,
+    Check_out varchar(255) NOT NULL,
+    Adult varchar(255) NOT NULL,
+    Child varchar(255) NOT NULL,
+    Room varchar(255) NOT NULL,
+    Request varchar(255)
+);
+INSERT INTO Booking
+VALUES ('abcxyz@gmail.com', 'VO TAN KIET', '11/29/2022 1:38 PM','11/29/2022 1:38 PM', 'Adult 1','Child 1','Room 1', 'YOLO');
+INSERT INTO Booking
+VALUES ('guest1@gmail.com', 'MIANMA ROCKET', '11/29/2022 1:38 PM','11/29/2022 1:38 PM', 'Adult 2','Child 2','Room 3', '');
+INSERT INTO Booking
+VALUES ('guestABC@gmail.com', 'SANTA', '11/29/2022 1:38 PM','11/29/2022 1:38 PM', 'Adult 2','Child 2','Room 3', 'HO HO HO');
---
+++ mvc/core/Database.php
@@ -0,0 +1,18 @@
+<?php
+class Databse{
+    public $connect;
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "";
+    protected $dbname = "hotel_db";
+
+    function __construct(){
+        $this->connect = mysqli_connect($this->servername, $this->username, $this->password);
+        mysqli_select_db($this->connect, $this->dbname);
+        mysqli_query($this->connect, "SET NAMES 'utf8'");
+    }
+}
+
+
+
+?>
