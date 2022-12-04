e7b6ec447b8a584dcb3d9362a91ebc2b161d2a91
phamthao3141592654 <96811325+phamthao3141592654@users.noreply.github.com>
2022-12-04 15:58:04 +0700

    thaopham
    room

--- mvc/controllers/Rooms.php
+++ mvc/controllers/Rooms.php
@@ -38,28 +38,6 @@ class Rooms extends Controller{
             "rooms"=>$rooms
         ]);
     }
-<<<<<<< Updated upstream
-    // public function getBooking(){
-    //     if(isset($_POST['booking'])){
-    //         $name = $_POST['name'];
-    //         $email = $_POST['email'];
-    //         $checkin = $_POST['checkin'];
-    //         $checkout = $_POST['checkout'];
-    //         $adult = $_POST['adult'];
-    //         $child = $_POST['child'];
-    //         $room = $_POST['room'];
-    //         $request = $_POST['request'];
-    //         $kq = $this->model("BookingModel")->AddBooking($name, $email, $checkin, $checkout, $adult, $child, $room, $request);
-    //         if ($kq){
-    //             $this->alert("Đặt phòng thành công!!!");
-    //         }
-    //         else {
-    //             $this->alert("Đặt phòng thất bại...Mời nhập lại");
-    //         }
-    //     }
-    //     $this->show();
-    // }
-=======
     public function addRoom(){
         if(isset($_POST['add_Room'])){
             $roomType = $_POST['room_type'];
@@ -133,7 +111,6 @@ class Rooms extends Controller{
         }
         $this->manage_rooms();
     }
->>>>>>> Stashed changes
 }
 
 ?>
--- mvc/views/admin/booking.php
+++ mvc/views/admin/booking.php
@@ -53,11 +53,7 @@
                                 </button>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#">Edit Profile</a></li>
-<<<<<<< Updated upstream
-                                    <li><a class="dropdown-item" href="http://localhost/Web_Assignment/Booking/viewAllBooking">Manage Booking</a></li>
-=======
                                     <li><a class="dropdown-item" href="http://localhost/Web_Assignment/Booking/manage_booking">Manage Booking</a></li>
->>>>>>> Stashed changes
                                     <li><a class="dropdown-item" href="http://localhost/Web_Assignment/Rooms/manage_rooms">Manage Rooms</a></li>
                                     <li><a class="dropdown-item" href="#">Manage News</a></li>
                                     <li><a class="dropdown-item" href="#">Manage Admin</a></li>
